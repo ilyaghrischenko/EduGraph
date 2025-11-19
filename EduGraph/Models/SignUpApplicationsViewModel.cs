c66f6912c3b899c526f1f65cccfaf501caa67099
Illia <144327680+ilyaghrischenko@users.noreply.github.com>
2025-11-19 17:55:18 +0200

    style(models): replace object initializer with explicit array initialization

--- EduGraph/Models/SignUpApplicationsViewModel.cs
+++ EduGraph/Models/SignUpApplicationsViewModel.cs
@@ -2,7 +2,7 @@ namespace EduGraph.Models;
 
 public sealed class SignUpApplicationsViewModel
 {
-    public List<EduGraph.Entities.SignUpApplication> Applications { get; set; } = new();
+    public List<EduGraph.Entities.SignUpApplication> Applications { get; set; } = [];
     public int CurrentPage { get; set; } = 1;
     public int TotalPages { get; set; } = 1;
     
